3a785508c681a3e200b66c9540828efb5d2a481a
John Bannister <johnb@hey.com>
2023-09-08 11:57:17 +0100

    Setup indentation for lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -34,10 +34,10 @@ require("lazy").setup({
     config = function ()
       require("nvim-treesitter.configs").setup({
         ensure_installed = { "fish", "lua", "go", "javascript", "query", "typescript", "vim", "vimdoc" },
-	sync_install = false,
+        sync_install = false,
         auto_install = false,
-	highlight = { enable = true },
-	indent = { enable = true },
+        highlight = { enable = true },
+        indent = { enable = true },
       })
     end
   },
