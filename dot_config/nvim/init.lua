7f334d8365f186bea4da18a56e48ea894e8f66b0
John Bannister <johnb@hey.com>
2023-09-10 13:19:31 +0100

    Add telescope plugin

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -96,6 +96,17 @@ require("lazy").setup({
       }
     },
   },
+
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    keys = {
+      { "<c-p>", "<cmd>Telescope find_files<cr>", mode = "n", desc = "Search project files" },
+      { "<leader>f", "<cmd>Telescope<cr>", mode = "n", desc = "Search Telescope built-in pickers" },
+      { "<leader>fh", "<cmd>Telescope help_tags<cr>", mode = "n", desc = "Search help tags" },
+      { "<leader>fg", "<cmd>Telescope live_grep<cr>", mode = "n", desc = "Search contents of project files" },
+    },
+  },
 })
 
 require("lsp")
