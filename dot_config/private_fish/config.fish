b75a7fe215115756ed51aa94962d0886a01fbaa2
John Bannister <johnb@hey.com>
2023-09-10 09:50:46 +0100

    Attempt to fix homebrew shellenv across platforms

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,6 +1,9 @@
 # Setup homebrew
-set brew_path (which brew)
-eval ($brew_path shellenv fish)
+if test -f /opt/homebrew/bin/brew
+    eval (/opt/homebrew/bin/brew shellenv fish)
+else if test -f /usr/local/homebrew/bin/brew
+    eval (/usr/local/homebrew/bin/brew shellenv fish)
+end
 
 # Setup rtx
 eval "$(rtx activate fish)"
