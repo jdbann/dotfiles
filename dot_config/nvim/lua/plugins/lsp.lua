790ea971756039b81853f08abe06b3e75e72fc84
John Bannister <johnb@hey.com>
2023-09-19 17:21:10 +0100

    Extract lsp plugin config file

---
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      vim.list_extend(opts.sources, {
+        nls.builtins.diagnostics.golangci_lint,
+      })
+    end,
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        gopls = {
+          settings = {
+            gopls = {
+              analyses = {
+                fieldalignment = false,
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}
--- dot_config/nvim/lua/plugins/plugins.lua
+++ dot_config/nvim/lua/plugins/plugins.lua
@@ -13,33 +13,6 @@ return {
   },
 
   {
-    "jose-elias-alvarez/null-ls.nvim",
-    opts = function(_, opts)
-      local nls = require("null-ls")
-      vim.list_extend(opts.sources, {
-        nls.builtins.diagnostics.golangci_lint,
-      })
-    end,
-  },
-
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        gopls = {
-          settings = {
-            gopls = {
-              analyses = {
-                fieldalignment = false,
-              },
-            },
-          },
-        },
-      },
-    },
-  },
-
-  {
     "nvim-neotest/neotest",
     dependencies = {
       { "nvim-neotest/neotest-go" },
