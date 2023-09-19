928fec8d01f6cde84f768517a07302f7cead030a
John Bannister <johnb@hey.com>
2023-09-19 12:15:13 +0100

    Match lualine style to tmux bubbles

--- dot_config/nvim/lua/plugins/plugins.lua
+++ dot_config/nvim/lua/plugins/plugins.lua
@@ -12,19 +12,6 @@ return {
   },
 
   {
-    "akinsho/bufferline.nvim",
-    opts = {
-      options = {
-        separator_style = "slant",
-        hover = {
-          enabled = true,
-          reveal = { "close" },
-        },
-      },
-    },
-  },
-
-  {
     "jose-elias-alvarez/null-ls.nvim",
     opts = function(_, opts)
       local nls = require("null-ls")
@@ -63,41 +50,6 @@ return {
         "neotest-jest",
       },
     },
-  },
-
-  {
-    "folke/edgy.nvim",
-    opts = function(_, opts)
-      local function remove(tbl, func)
-        local i = 1
-        while i <= #tbl do
-          if func(tbl[i]) then
-            i = i + 1
-          else
-            table.remove(tbl, i)
-          end
-        end
-      end
-
-      remove(opts.left, function(window)
-        return window.title == "Neo-Tree Git"
-      end)
-      remove(opts.left, function(window)
-        return window.title == "Neo-Tree Buffers"
-      end)
-      remove(opts.left, function(window)
-        return window.title == "Neotest Summary"
-      end)
-
-      return vim.tbl_deep_extend("force", opts, {
-        animate = {
-          enabled = false,
-        },
-        right = {
-          { title = "Neotest Summary", ft = "neotest-summary", wo = { winfixwidth = false } },
-        },
-      })
-    end,
   },
 
   {
