1aded8c22c7ffb764651cb49055d51267ddcfadc
John Bannister <johnb@hey.com>
2023-10-09 09:46:24 +0100

    Switch to none-ls

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -1,15 +1,18 @@
 return {
   {
-    "jose-elias-alvarez/null-ls.nvim",
-    opts = function(_, opts)
-      local nls = require("null-ls")
-      vim.list_extend(opts.sources, {
-        nls.builtins.diagnostics.golangci_lint,
-      })
-    end,
-  },
+    {
+      "nvimtools/none-ls.nvim",
+      optional = true,
+      opts = function(_, opts)
+        if type(opts.sources) == "table" then
+          local nls = require("null-ls")
+          vim.list_extend(opts.sources, {
+            nls.builtins.diagnostics.golangci_lint,
+          })
+        end
+      end,
+    },
 
-  {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
